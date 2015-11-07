04b9a5a5b7f6f89a7df6d198b137772dd6fcbe85
loujine <none@none>
2015-11-07 23:49:18 +0000

    [replace recording artist] Refactor

--- mbz-showperformers.user.js
+++ mbz-showperformers.user.js
@@ -19,73 +19,8 @@
 // @run-at       document-end
 // ==/UserScript==
 
-// imported from mbz-loujine-common.js: requestGET, mbzTimeout
-
-function formatPerformers(relations) {
-    var performers = [];
-    relations.forEach(function(rel) {
-        var type;
-        if (rel.type === 'instrument' || rel.type === 'vocal' ||
-            rel.type === 'conductor' || rel.type === 'performing orchestra' ||
-            rel.type === 'performer') {
-            if (rel.type === 'performing orchestra') {
-                type = 'orchestra';
-            } else if (!rel.attributes.length) {
-                type = rel.type;
-            } else {
-                type = rel.attributes[0];
-            }
-            performers.push(type + ': ' + rel.artist.name);
-        }
-    });
-    return performers.sort().join(', ');
-}
-
-function formatEditInfo(json) {
-    var data = [],
-        performers = [],
-        editNote;
-    data.push('edit-recording.name=' + json.name);
-    if (!json.comment.length) {
-        data.push('edit-recording.comment');
-    } else {
-        data.push('edit-recording.comment=' + json.comment);
-    }
-    if (!json.isrcs.length) {
-        data.push('edit-recording.isrcs.0');
-    } else {
-        json.isrcs.forEach(function(isrc, idx) {
-            data.push('edit-recording.isrcs.' + idx + '=' + json.isrc);
-        });
-    }
-    json.relationships.forEach(function(rel, idx) {
-        var linkType = rel.linkTypeID;
-        if (linkType === linkTypePerformer ||
-            linkType === linkTypeInstrument || linkType === linkTypeVocals ||
-            linkType === linkTypeOrchestra || linkType === linkTypeConductor) {
-            performers.push({'name': rel.target.name, 'id': rel.target.id});
-        }
-        data.push('edit-recording.rel.' + idx + '.relationship_id=' + rel.id);
-        data.push('edit-recording.rel.' + idx + '.target=' + rel.target.gid);
-        data.push('edit-recording.rel.' + idx + '.link_type_id=' + linkType);
-        if (linkType === 278) {
-            data.push('edit-recording.rel.' + idx + '.backward=1');
-        }
-    });
-    editNote = $('#batch_replace_edit_note')[0].value;
-    data.push('edit-recording.edit_note=' + editNote);
-    performers.forEach(function(performer, idx) {
-        data.push('edit-recording.artist_credit.names.' + idx + '.name=' + performer.name);
-        if (idx === performers.length - 1) {
-            data.push('edit-recording.artist_credit.names.' + idx + '.join_phrase');
-        } else {
-            data.push('edit-recording.artist_credit.names.' + idx + '.join_phrase=,+');
-        }
-        data.push('edit-recording.artist_credit.names.' + idx + '.artist.name=' + performer.name);
-        data.push('edit-recording.artist_credit.names.' + idx + '.artist.id=' + performer.id);
-    });
-    return data.join('&').replace(/ /g, '+');
-}
+// imported from mbz-loujine-common.js: requestGET, mbzTimeout,
+// formatPerformers, replaceArtist
 
 function showPerformers() {
     var composer = $('th:contains("composer:")').parent().find('a').attr('href').split('/')[4],
@@ -118,35 +53,6 @@ function showPerformers() {
                 $(tr).append($node);
             });
         }, idx * mbzTimeout);
-    });
-}
-
-function replaceArtist() {
-    // in order to determine the edit parameters required by POST
-    // we first load the /edit page and parse the JSON data
-    // in the sourceData block
-    $('.replace:input:checked:enabled').each(function (idx, node) {
-        setTimeout(function () {
-            var mbid = node.id.replace('replace-', ''),
-                url = '/recording/' + encodeURIComponent(mbid) + '/edit',
-                callback = function (info) {
-                    console.log('Sending POST ' + mbid + ' edit info');
-                    requestPOST(url, formatEditInfo(info), function (status) {
-                        node.disabled = true;
-                        if (status === 200) {
-                            $(node).parent().css('color', 'green');
-                        } else {
-                            $(node).after(status).parent().css('color', 'red');
-                        }
-                    });
-                };
-            console.log('Fetching ' + mbid + ' edit info');
-
-            requestGET(url, function (resp) {
-                var info = new RegExp('sourceData: (.*),\n').exec(resp)[1];
-                callback(JSON.parse(info));
-            });
-        }, 2 * idx * mbzTimeout);
     });
 }
 
