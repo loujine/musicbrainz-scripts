1e74484f78ba25bb0824cf1fdd6db7e55e368c17
loujine <none@none>
2016-12-11 15:21:58 +0000

    [show missing works] Apply script on artist/mbid/recordings page (close #7)

--- mbz-showmissingworks.user.js
+++ mbz-showmissingworks.user.js
@@ -1,19 +1,20 @@
 /* global $ requests server sidebar helper */
 'use strict';
 // ==UserScript==
-// @name         MusicBrainz: Show missing works
+// @name         MusicBrainz: Show missing works on an artist page
 // @namespace    mbz-loujine
 // @author       loujine
-// @version      2016.6.25
+// @version      2016.12.11
 // @downloadURL  https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/mbz-showmissingworks.user.js
 // @updateURL    https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/mbz-showmissingworks.user.js
 // @supportURL   https://bitbucket.org/loujine/musicbrainz-scripts
 // @icon         https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/icon.png
-// @description  musicbrainz.org: Mark recordings not linked to any work on a performer page
+// @description  musicbrainz.org: Mark recordings not linked to any work on an artist recordings or relationships page
 // @compatible   firefox+greasemonkey
 // @licence      CC BY-NC-SA 3.0 (https://creativecommons.org/licenses/by-nc-sa/3.0/)
 // @require      https://greasyfork.org/scripts/13747-mbz-loujine-common/code/mbz-loujine-common.js?version=133551
 // @include      http*://*musicbrainz.org/artist/*/relationships
+// @include      http*://*musicbrainz.org/artist/*/recordings*
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
