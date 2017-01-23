417b1d90adac04d6913a55e0e89990faacb1b10b
loujine <none@none>
2017-01-23 23:22:57 +0000

    [sort columns] Update description

--- mbz-sortablecolumns.user.js
+++ mbz-sortablecolumns.user.js
@@ -1,7 +1,7 @@
 /* global $ sidebar */
 'use strict';
 // ==UserScript==
-// @name         MusicBrainz: Make table columns sortable (on Work pages)
+// @name         MusicBrainz: Make table columns sortable
 // @namespace    mbz-loujine
 // @author       loujine
 // @version      2017.1.23
@@ -9,7 +9,7 @@
 // @updateURL    https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/mbz-sortablecolumns.user.js
 // @supportURL   https://bitbucket.org/loujine/musicbrainz-scripts
 // @icon         https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/icon.png
-// @description  musicbrainz.org: Make table columns sortable (on Work pages)
+// @description  musicbrainz.org: Make table columns sortable (tested on Work pages)
 // @compatible   firefox+greasemonkey
 // @licence      CC BY-NC-SA 3.0 (https://creativecommons.org/licenses/by-nc-sa/3.0/)
 // @require      https://greasyfork.org/scripts/13747-mbz-loujine-common/code/mbz-loujine-common.js?version=171262
