8480e3534650f4a05ce18144ada1f358029e87d9
loujine <loujine@protonmail.com>
2018-01-03 00:48:42 +0000

    [set attributes] Add instrumental attribute button
    Also cleanup to simplify jquery calls

--- mbz-setattributes.user.js
+++ mbz-setattributes.user.js
@@ -4,7 +4,7 @@
 // @name         MusicBrainz: Set relation attributes in relationships editor
 // @namespace    mbz-loujine
 // @author       loujine
-// @version      2017.12.9
+// @version      2017.12.29
 // @downloadURL  https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/mbz-setattributes.user.js
 // @updateURL    https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/mbz-setattributes.user.js
 // @supportURL   https://bitbucket.org/loujine/musicbrainz-scripts
@@ -19,17 +19,17 @@
 // ==/UserScript==
 
 function setAttributes(relationType, attrId, toggle) {
-    var recordings = MB.relationshipEditor.UI.checkedRecordings();
+    const recordings = MB.relationshipEditor.UI.checkedRecordings();
     recordings.forEach(function(recording) {
         recording.relationships().forEach(function(relation) {
             if (relation.entityTypes !== relationType) {
                 return;
             }
             var attrs = relation.attributes(),
-                attr = attrs.filter(function(el) {
+                attr = attrs.filter(
                     // attrId already in relation attributes
-                    return el.type.id === attrId;
-                });
+                    el => el.type.id === attrId
+                );
             if (!attr.length) {
                 attrs.push({type: MB.attrInfoByID[attrId]});
             } else if (toggle) {
@@ -43,62 +43,32 @@ function setAttributes(relationType, attrId, toggle) {
 
 (function displayToolbar(relEditor) {
     $('div.tabs').after(
-        relEditor.container().append(
-            $('<h3>', {'text': 'Recording-Work relation attributes'})
-        ).append(
-            $('<input>', {
-                'id': 'setlive',
-                'type': 'button',
-                'value': 'Set live'
-            })
-        ).append(
-            $('<input>', {
-                'id': 'setpartial',
-                'type': 'button',
-                'value': 'Set partial'
-            })
-        ).append(
-            $('<input>', {
-                'id': 'togglelive',
-                'type': 'button',
-                'value': 'Toggle live'
-            })
-        ).append(
-            $('<input>', {
-                'id': 'togglepartial',
-                'type': 'button',
-                'value': 'Toggle partial'
-            })
-        ).append(
-            $('<h3>', {'text': 'Recording-Artist relation attributes'})
-        ).append(
-            $('<input>', {
-                'id': 'togglesolo',
-                'type': 'button',
-                'value': 'Toggle solo'
-            })
-        )
+        relEditor.container().append(`
+            <h3>Recording-Work relation attributes</h3>
+            <input type="button" id="setLive" value="Set live">
+            <input type="button" id="setPartial" value="Set partial">
+            <input type="button" id="setInstrumental" value="Set instrumental">
+            <input type="button" id="toggleLive" value="Toggle live">
+            <input type="button" id="togglePartial" value="Toggle partial">
+            <input type="button" id="toggleInstrumental" value="Toggle instrumental">
+            <h3>Recording-Artist relation attributes</h3>
+            <input type="button" id="toggleSolo" value="Toggle solo">
+        `)
     );
 })(relEditor);
 
 $(document).ready(function() {
-    $('#setlive').click(function() {
-        setAttributes('recording-work', server.attr.live, false);
-        relEditor.editNote(GM_info.script);
-    });
-    $('#setpartial').click(function() {
-        setAttributes('recording-work', server.attr.partial, false);
-        relEditor.editNote(GM_info.script);
-    });
-    $('#togglelive').click(function() {
-        setAttributes('recording-work', server.attr.live, true);
-        relEditor.editNote(GM_info.script);
-    });
-    $('#togglepartial').click(function() {
-        setAttributes('recording-work', server.attr.partial, true);
-        relEditor.editNote(GM_info.script);
-    });
-    $('#togglesolo').click(function() {
+    for (let attr of ['Live', 'Partial', 'Instrumental']) {
+        document.getElementById(`set${attr}`).addEventListener('click', () => {
+            setAttributes('recording-work', server.attr[attr.toLowerCase()], false);
+            relEditor.editNote(GM_info.script);
+        });
+        document.getElementById(`toggle${attr}`).addEventListener('click', () => {
+            setAttributes('recording-work', server.attr[attr.toLowerCase()], true);
+            relEditor.editNote(GM_info.script);
+        });
+    }
+    document.getElementById(`toggleSolo`).addEventListener('click', () => {
         setAttributes('artist-recording', server.attr.solo, true);
         relEditor.editNote(GM_info.script);
     });
