7e3faa4650a993637e532e65af6a46dd898a8ee9
loujine <loujine@protonmail.com>
2018-01-07 00:20:17 +0000

    [stats ABrainz] Clean and remove jquery dependency

--- mbz-show_stats_from_acousticbrainz.user.js
+++ mbz-show_stats_from_acousticbrainz.user.js
@@ -1,10 +1,10 @@
-/* global $ _ requests sidebar helper */
+/* global _ sidebar helper */
 'use strict';
 // ==UserScript==
 // @name         MusicBrainz: Show stats from AcousticBrainz
 // @namespace    mbz-loujine
 // @author       loujine
-// @version      2018.1.5
+// @version      2018.1.7
 // @downloadURL  https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/mbz-show_stats_from_acousticbrainz.user.js
 // @updateURL    https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/mbz-show_stats_from_acousticbrainz.user.js
 // @supportURL   https://bitbucket.org/loujine/musicbrainz-scripts
@@ -21,38 +21,33 @@
 // ==/UserScript==
 
 function showAcousticBrainzCount() {
-    var mbid = helper.mbidFromURL();
-    var countUrl = '//acousticbrainz.org/api/v1/' + mbid + '/count';
-    var dataUrl = '//acousticbrainz.org/api/v1/' + mbid + '/low-level';
-
-    requests.GET(countUrl, function (resp) {
-        var count = JSON.parse(resp).count;
-        $('#count').append(count);
+    const mbid = helper.mbidFromURL();
+    fetch(`//acousticbrainz.org/api/v1/${mbid}/count`).then(
+        resp => resp.json()
+    ).then(json => {
+        const count = json.count;
+        document.getElementById('ABcount').append(count);
         if (count > 0) {
-            requests.GET(dataUrl, function (resp) {
-                var data = JSON.parse(resp);
-                var key = data.tonal.key_key + ' ' + data.tonal.key_scale;
-                key += ' (' + _.round(100 * data.tonal.key_strength, 1) + '%)';
-                $('#key').append(key);
-                var bpm = _.round(data.rhythm.bpm, 1);
-                $('#bpm').append(bpm);
+            fetch(`//acousticbrainz.org/api/v1/${mbid}/low-level`).then(
+                resp => resp.json()
+            ).then(data => {
+                document.getElementById('ABkey').append(
+                    `${data.tonal.key_key} ${data.tonal.key_scale} (${_.round(100 * data.tonal.key_strength, 1)}%)`
+                );
+                document.getElementById('ABbpm').append(_.round(data.rhythm.bpm, 1));
             });
         }
     });
 }
 
-(function displaySidebar() {
+(function main() {
     const mbid = helper.mbidFromURL();
     sidebar.container().insertAdjacentHTML('beforeend', `
         <h3>Show statistics</h3>
         <a href="//acousticbrainz.org/${mbid} target="_blank">AcousticBrainz entry</a>
-        <p id="count">Number of submissions:&nbsp;</p>
-        <p id="key">Key:&nbsp;</p>
-        <p id="bpm">BPM:&nbsp;</p>
+        <p id="ABcount">Number of submissions:&nbsp;</p>
+        <p id="ABkey">Key:&nbsp;</p>
+        <p id="ABbpm">BPM:&nbsp;</p>
     `);
-})();
-
-$(document).ready(function() {
     showAcousticBrainzCount();
-    return false;
-});
+})();
