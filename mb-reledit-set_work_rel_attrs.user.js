966a781fd3f5024a543a1f2dec19f485778d4185
loujine <loujine@protonmail.com>
2018-01-11 00:17:02 +0000

    [reledit set attr] Prepare script renaming

--- mb-reledit-set_work_rel_attrs.user.js
+++ mb-reledit-set_work_rel_attrs.user.js
@@ -1,12 +1,12 @@
 /* global $ MB server relEditor GM_info */
 'use strict';
 // ==UserScript==
-// @name         MusicBrainz relation editor: Set recording-work relation attributes
+// @name         MusicBrainz relation editor: Set relation attributes
 // @namespace    mbz-loujine
 // @author       loujine
-// @version      2018.1.8
-// @downloadURL  https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/mb-reledit-set_work_rel_attrs.user.js
-// @updateURL    https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/mb-reledit-set_work_rel_attrs.user.js
+// @version      2018.1.9
+// @downloadURL  https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/mb-reledit-set_relation_attrs.user.js
+// @updateURL    https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/mb-reledit-set_relation_attrs.user.js
 // @supportURL   https://bitbucket.org/loujine/musicbrainz-scripts
 // @icon         https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/icon.png
 // @description  musicbrainz.org relation editor: Set attributes (live, partial, solo...)
