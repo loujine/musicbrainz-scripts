70689d1d6d43bba367103fbf54526ac66137366a
loujine <loujine@protonmail.com>
2018-07-16 20:27:24 +0000

    [event setlist] Go back to simple input UI
    contextmenu is not working in Chromium

--- mb-edit-fill_event_setlist.user.js
+++ mb-edit-fill_event_setlist.user.js
@@ -1,10 +1,10 @@
-/* global $  */
+/* global $ GM_info sidebar relEditor */
 'use strict';
 // ==UserScript==
 // @name         MusicBrainz event editor: Fill event setlist
 // @namespace    mbz-loujine
 // @author       loujine
-// @version      2018.7.15
+// @version      2018.7.16
 // @downloadURL  https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/mb-edit-fill_event_setlist.user.js
 // @updateURL    https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/mb-edit-fill_event_setlist.user.js
 // @supportURL   https://bitbucket.org/loujine/musicbrainz-scripts
@@ -12,52 +12,62 @@
 // @description  musicbrainz.org event editor: Fill event setlist
 // @compatible   firefox+tampermonkey
 // @license      MIT
+ // @require      https://greasyfork.org/scripts/13747-mbz-loujine-common/code/mbz-loujine-common.js?version=612784
 // @include      http*://*musicbrainz.org/event/*/edit
 // @include      https://musicbrainz.org/event/create
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
 
-$(document).ready(function() {
+(function displayToolbar() {
+    document.getElementsByClassName('half-width')[0].insertAdjacentHTML(
+        'afterend', '<div id="side-col" style="float: right;"></div>');
+    relEditor.container(document.getElementById('side-col')).insertAdjacentHTML(
+        'beforeend', `
+        <h3>Complete entity in setlist</h3>
+        <input type="button" id="fillArtist" value="Fill artist MBID">
+        <input type="button" id="fillWork" value="Fill work MBID">
+    `);
+    document.getElementById('loujine-menu').style.marginLeft = '550px';
+})();
+
+function fillEventSetlist(entityType) {
     const area = document.getElementById("id-edit-event.setlist");
-    area.setAttribute('contextmenu', "popup-menu")
-    area.insertAdjacentHTML('afterend', `
-      <menu type="context" id="popup-menu">
-        <menuitem label="Replace by artist MBID"></menuitem>
-        <menuitem label="Replace by work MBID"></menuitem>
-      </menu>
-    `)
+    // document.getSelection() does not work on textarea
+    const entity = area.value.substring(area.selectionStart, area.selectionEnd);
+    fetch(`/ws/2/${entityType}?query=${entityType}:${entity}&limit=1&fmt=json`).then(
+        resp => resp.json()
+    ).then(
+        resp => {
+            const filledEntity = entityType === 'artist' ?
+                `@ [${resp.artists[0].id}|${resp.artists[0].name}]` :
+                `* [${resp.works[0].id}|${resp.works[0].title}]`;
+            area.value = area.value.replace(`/${entity}/g`, filledEntity);
+        }
+    )
+}
 
-    area.contextMenu.children[0].addEventListener(
-        'click',
+$(document).ready(function() {
+    let appliedNote = false;
+    document.getElementById('fillArtist').addEventListener('click',
         () => {
-            const str = area.value.substring(area.selectionStart, area.selectionEnd);
-            fetch(`/ws/2/artist?query=artist:${str}&limit=1&fmt=json`).then(
-                resp => resp.json()
-            ).then(
-                resp => {
-                    area.value = area.value.replace(
-                        str, `@ [${resp.artists[0].id}|${resp.artists[0].name}]`
-                    );
-                }
-            )
+            fillEventSetlist('work')
+            if (!appliedNote) {
+                document.getElementById('id-edit-event.edit_note')
+                    .value = sidebar.editNote(GM_info.script);
+                appliedNote = true;
+            }
         }
     );
-
-    area.contextMenu.children[1].addEventListener(
-        'click',
+    document.getElementById('fillWork').addEventListener('click',
         () => {
-            const str = area.value.substring(area.selectionStart, area.selectionEnd);
-            fetch(`/ws/2/work?query=work:${str}&limit=1&fmt=json`).then(
-                resp => resp.json()
-            ).then(
-                resp => {
-                    console.log(resp.works[0]);
-                    area.value = area.value.replace(
-                        str, `* [${resp.works[0].id}|${resp.works[0].title}]`
-                    );
-                }
-            )
+            fillEventSetlist('work')
+            if (!appliedNote) {
+                document.getElementById('id-edit-event.edit_note')
+                    .value = sidebar.editNote(GM_info.script);
+                appliedNote = true;
+            }
         }
     );
+    return false;
 });
