4a5b4599e4fdd2d64324e80ab793f873950cdbf6
loujine <loujine@protonmail.com>
2020-09-15 14:45:45 +0000

    [discid count] Mark script as obsolete
    Made obsolete by MBS-10568

--- mb-display_count_discid.user.js
+++ mb-display_count_discid.user.js
@@ -1,10 +1,10 @@
 /* global helper */
 'use strict';
 // ==UserScript==
-// @name         MusicBrainz: Display discid count
+// @name         (obsolete) MusicBrainz: Display discid count
 // @namespace    mbz-loujine
 // @author       loujine
-// @version      2020.9.14
+// @version      2020.9.15
 // @downloadURL  https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mb-display_count_discid.user.js
 // @updateURL    https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mb-display_count_discid.user.js
 // @supportURL   https://github.com/loujine/musicbrainz-scripts
@@ -12,7 +12,6 @@
 // @description  musicbrainz.org: Display discid count on main release pages
 // @compatible   firefox+tampermonkey
 // @license      MIT
-// @require      https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mbz-loujine-common.js
 // @include      http*://*musicbrainz.org/release/*
 // @exclude      http*://*musicbrainz.org/release/add*
 // @exclude      http*://*musicbrainz.org/release/*/*
@@ -20,18 +19,4 @@
 // @run-at       document-end
 // ==/UserScript==
 
-// adapted from jesus2099  mb. INLINE STUFF
-
-function parseCount(data, tab) {
-    const cnt = data.media.reduce((cnt, medium) => cnt + medium.discs.length, 0);
-    if (cnt > 0) {
-        tab.style.backgroundColor = '#6f9';
-    }
-    tab.append(` (${cnt})`);
-}
-
-(function showCountDiscid() {
-    const tab = document.querySelector("a[href$='/discids']");
-    const url = helper.wsUrl('release', ['discids']);
-    fetch(url).then(resp => resp.json()).then(data => parseCount(data, tab));
-})();
+// the info is now displayed directly in MusicBrainz server (MBS-10568)
