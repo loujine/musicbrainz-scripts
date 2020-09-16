75bb2e168d6335d9363f3b74cb1371e956371e6a
loujine <loujine@protonmail.com>
2020-09-16 22:57:22 +0000

    [set aliases] Downgrade script for Firefox < 45

--- mb-edit-add_aliases.user.js
+++ mb-edit-add_aliases.user.js
@@ -1,26 +1,75 @@
-/* global $ helper aliases edits sidebar requests */
+/* global $ */
 'use strict';
 // ==UserScript==
 // @name         MusicBrainz edit: Add entity aliases in batch
 // @namespace    mbz-loujine
 // @author       loujine
-// @version      2020.9.14
-// @downloadURL  https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mb-edit-add_aliases.user.js
-// @updateURL    https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mb-edit-add_aliases.user.js
+// @version      2020.9.17
+// @downloadURL  https://raw.githubusercontent.com/loujine/musicbrainz-scripts/aliases_for_ff44/mb-edit-add_aliases.user.js
+// @updateURL    https://raw.githubusercontent.com/loujine/musicbrainz-scripts/aliases_for_ff44/mb-edit-add_aliases.user.js
 // @supportURL   https://github.com/loujine/musicbrainz-scripts
 // @icon         https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/icon.png
 // @description  musicbrainz.org edit: Add entity aliases in batch
 // @compatible   firefox+tampermonkey
 // @license      MIT
-// @require      https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mbz-loujine-common.js
 // @include      http*://*musicbrainz.org/*/*/aliases*
 // @exclude      http*://*musicbrainz.org/doc/*
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
 
-const aliasType = helper.isArtistURL() ? aliases.artistType :
-                  helper.isInstrumentURL() ? aliases.instrumentType : aliases.type;
+function buildOptions(obj) {
+    return Object.keys(obj)
+        .map(type => `<option value="${obj[type]}">${type}</option>`)
+        .join('');
+}
+
+const server = {
+    aliasArtistType: {
+        'Artist name': 1,
+        'Legal name': 2,
+        'Search hint': 3,
+    },
+    aliasInstrumentType: {
+        'Instrument name': 1,
+        'Search hint': 2,
+        'Brand name': 3,
+    },
+    aliasType: {
+        'Name': 1,
+        'Search hint': 2,
+    },
+}
+
+const aliases = {
+    artistType: `
+        <select>
+          <option selected> </option>
+          ${buildOptions(server.aliasArtistType)}
+        </select>
+    `,
+    instrumentType: `
+        <select>
+          <option selected> </option>
+          ${buildOptions(server.aliasInstrumentType)}
+        </select>
+    `,
+    type: `
+        <select>
+          <option selected> </option>
+          ${buildOptions(server.aliasType)}
+        </select>
+    `,
+};
+
+const aliasType =
+    document.URL.split('/')[3] === 'artist'
+    ? aliases.artistType
+    : (
+        document.URL.split('/')[3] === 'instrument'
+        ? aliases.instrumentType
+        : aliases.type
+    );
 
 function addRow() {
     document.querySelector('tbody tr:last-child').insertAdjacentHTML('afterend', `
@@ -43,25 +92,76 @@ function addRow() {
     });
 }
 
+function edits_encodeName(name) {
+    return encodeURIComponent(name).replace(/%20/g, '+');
+}
+
+function sidebar_editNote(meta, msg) {
+    msg = msg || '';
+    const signature = `\n —\nGM script: "${meta.name}" (${meta.version})`;
+    return msg + signature;
+}
+
+function edits_formatEdit(editType, info) {
+    return Object.keys(info)
+        .map(prop =>
+            info[prop] === null
+            ? `${editType}.${prop}`
+            : `${editType}.${prop}=${info[prop]}`
+        )
+        .join('&');
+}
+
+function _request(verb, url, param, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            callback(xhr); // eslint-disable-line callback-return
+        }
+    };
+    xhr.open(verb, url, true);
+    if (verb === 'POST') {
+        xhr.setRequestHeader(
+            'Content-Type',
+            'application/x-www-form-urlencoded'
+        );
+    }
+    xhr.timeout = 10000;
+    xhr.ontimeout = function () {
+        console.error('The request for ' + url + ' timed out.');
+    };
+    xhr.send(param);
+}
+
+function requests_POST(url, param, successCallback, failCallback) {
+    _request('POST', url, param, function (xhr) {
+        if (xhr.status === 200 || xhr.status === 0) {
+            successCallback(xhr);
+        } else {
+            failCallback(xhr);
+        }
+    });
+}
+
 function submitAliases() {
-    for (const node of document.getElementsByClassName('newAlias')) {
+    document.getElementsByClassName('newAlias').forEach(node => {
         const cols = node.children;
         const postData = {
-            name: edits.encodeName(cols[0].children[0].value),
-            sort_name: edits.encodeName(cols[1].children[0].value),
+            name: edits_encodeName(cols[0].children[0].value),
+            sort_name: edits_encodeName(cols[1].children[0].value),
             type_id: cols[4].children[0].value,
             locale: cols[5].children[0].value,
             primary_for_locale: cols[5].children[1].checked ? 1 : 0,
-            edit_note: sidebar.editNote(GM_info.script),
+            edit_note: sidebar_editNote(GM_info.script),
         };
         if (postData.sort_name === '') {
             postData.sort_name = postData.name;
         }
         cols[6].textContent = 'Sending edit data';
         console.info('Data ready to be posted: ', postData);
-        requests.POST(
+        requests_POST(
             document.URL.replace('aliases', 'add-alias'),
-            edits.formatEdit('edit-alias', postData),
+            edits_formatEdit('edit-alias', postData),
             // success
             xhr => {
                 cols[6].textContent = `Success (code ${xhr.status})`;
@@ -74,12 +174,12 @@ function submitAliases() {
             }
         );
         node.classList.remove('newAlias');
-    }
+    });
 }
 
 $(document).ready(function () {
     // doesn't work on translated pages
-    for (const node of document.getElementById('content').getElementsByTagName('p')) {
+    document.getElementById('content').getElementsByTagName('p').forEach(node => {
         if (node.innerHTML.includes('has no aliases')) {
             node.innerHTML = `
                 <table class="tbl">
@@ -98,7 +198,7 @@ $(document).ready(function () {
                   </tbody>
                 </table>`;
         }
-    }
+    });
     document.getElementsByTagName('table')[0].insertAdjacentHTML('beforebegin', `
         <h3>Add aliases manually</h3>
         <input type="button" id="addRow" value="+ Add a new row">
