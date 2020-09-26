0e42f1712cf3d022da2e1b0a578839105a5e3c2d
loujine <loujine@protonmail.com>
2020-09-26 21:17:02 +0000

    [acousticbrainz] Improve fetching AB data

--- mb-display_acousticbrainz_dataset_for_work.user.js
+++ mb-display_acousticbrainz_dataset_for_work.user.js
@@ -4,7 +4,7 @@
 // @name         MusicBrainz: Display AcousticBrainz datasets count for work
 // @namespace    mbz-loujine
 // @author       loujine
-// @version      2020.9.23
+// @version      2020.9.26
 // @downloadURL  https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mb-display_acousticbrainz_dataset_for_work.user.js
 // @updateURL    https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mb-display_acousticbrainz_dataset_for_work.user.js
 // @supportURL   https://github.com/loujine/musicbrainz-scripts
@@ -21,6 +21,7 @@
 // ==/UserScript==
 
 const abzIconURL = '//acousticbrainz.org/static/images/favicon-16.png';
+const MAX_ITEMS_PER_BULK_REQUEST = 25;
 
 function showABids() {
     if (!$('thead > tr').length) {
@@ -30,38 +31,39 @@ function showABids() {
     const recording_mbids = Array.from($recordings).map(
         node => node.href.split('/')[4]
     );
-    if (recording_mbids.length > 25) {
-        console.info(
-            'Warning: sending only the first 25 recordings ' +
-                'to AcousticBrainz'
-        );
-        recording_mbids.splice(25);
-    }
-    const url =
-        '//acousticbrainz.org/api/v1/count?recording_ids=' +
-        recording_mbids.join(';');
     $('thead > tr').append('<th>ABrainz</th>');
     $('.subh > th')[1].colSpan += 1;
     $('table.tbl > tbody > tr:not(".subh")').append('<td>');
 
-    requests.GET(url, function (data) {
-        data = JSON.parse(data);
-        $recordings.each(function (idx, recording) {
-            const mbid = helper.mbidFromURL(recording.href);
-            if (data[mbid] === undefined || data[mbid].count === 0) {
-                return;
-            }
-            $(recording).parents('tr').find('td:last').append(
-                $('<a>', {
-                    'href': '//acousticbrainz.org/' + mbid,
-                    'target': '_blank',
-                    'text': data[mbid].count
-                }).append($('<img src="' + abzIconURL + '" class="abz" />'))
-            );
-        });
-    });
+    let mbid_batch;
+    let idx = 0;
+    while (recording_mbids.length) {
+        mbid_batch = recording_mbids.splice(0, MAX_ITEMS_PER_BULK_REQUEST);
+        const url =
+            '//acousticbrainz.org/api/v1/count?recording_ids=' +
+            mbid_batch.join(';');
+        setTimeout(() => {
+            requests.GET(url, (data) => {
+                data = JSON.parse(data);
+                console.log(`Fetching data for ${Object.keys(data).length - 1} MBIDs`);
+                $recordings.each((_, recording) => {
+                    const mbid = helper.mbidFromURL(recording.href);
+                    if (data[mbid] === undefined || data[mbid].count === 0) {
+                        return;
+                    }
+                    $(recording).parents('tr').find('td:last').append(
+                        $('<a>', {
+                            'href': '//acousticbrainz.org/' + mbid,
+                            'target': '_blank',
+                            'text': data[mbid].count,
+                        }
+                    ).append($('<img src="' + abzIconURL + '" class="abz" />')));
+                });
+            });
+        }, 1500 * idx);
+        idx += 1;
+    }
 }
-
 
 (function displaySidebar() {
     sidebar.container().insertAdjacentHTML('beforeend', `
