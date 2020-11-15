loujine/musicbrainz-scripts

commit 77aa23951985376ab3b3a286cb4cef65f0c42128

Remove Lodash
Lodash has been dropped from MB and this breaks some scripts

mb-edit-create_from_wikidata.user.js
@@ -1,10 +1,10 @@
-/* global $ _ helper relEditor sidebar GM_info GM_xmlhttpRequest */
+/* global $ helper relEditor sidebar GM_info GM_xmlhttpRequest */
 'use strict';
 // ==UserScript==
 // @name         MusicBrainz edit: Create entity or fill data from wikipedia / wikidata / VIAF / ISNI
 // @namespace    mbz-loujine
 // @author       loujine
-// @version      2020.8.17
+// @version      2020.9.8
 // @downloadURL  https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mb-edit-create_from_wikidata.user.js
 // @updateURL    https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mb-edit-create_from_wikidata.user.js
 // @supportURL   https://github.com/loujine/musicbrainz-scripts
@@ -295,12 +295,12 @@ const FIELD_NAMES = {
     'area': 'Area',
 };
 
-_.forOwn(FIELD_NAMES, (v, k) => {
-    if (k.includes('artist')) {
-        FIELD_NAMES[k.replace('artist', 'place')] = v;
-        FIELD_NAMES[k.replace('artist', 'work')] = v;
+for (const [key, value] of Object.entries(FIELD_NAMES)) {
+    if (key.includes('artist')) {
+        FIELD_NAMES[key.replace('artist', 'place')] = value;
+        FIELD_NAMES[key.replace('artist', 'work')] = value;
     }
-});
+}
 
 
 function setValue(nodeId, value, callback) {
@@ -540,7 +540,7 @@ function _fillFormFromWikidata(entity, entityType) {
     }
 
     const existingDomains = _existingDomains();
-    _.forOwn(libWD.urls, (url, externalLink) => {
+    for (const [externalLink, url] of Object.entries(libWD.urls)) {
         const domain = url.split('/')[2];
         if (!libWD.existField(entity, externalLink)) {
             return;
@@ -551,7 +551,7 @@ function _fillFormFromWikidata(entity, entityType) {
             (!domain && !existingDomains.includes(url.split('/')[2]))) {
             _fillExternalLinks(url);
         }
-    });
+    }
 
     for (const role of ['student', 'teacher']) {
         if (libWD.existField(entity, role)) {

mb-display_acousticbrainz_data_for_recording.user.js
@@ -1,10 +1,10 @@
-/* global _ sidebar helper */
+/* global sidebar helper */
 'use strict';
 // ==UserScript==
 // @name         MusicBrainz: Display AcousticBrainz data on recording page
 // @namespace    mbz-loujine
 // @author       loujine
-// @version      2019.9.22
+// @version      2020.9.8
 // @downloadURL  https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mbz-display_acousticbrainz_data_for_recording.user.js
 // @updateURL    https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mbz-display_acousticbrainz_data_for_recording.user.js
 // @supportURL   https://github.com/loujine/musicbrainz-scripts
@@ -20,6 +20,11 @@
 // @run-at       document-end
 // ==/UserScript==
 
+const round = (num, precision) => {
+  const modifier = 10 ** precision
+  return Math.round(num * modifier) / modifier
+}
+
 function showAcousticBrainzData() {
     const mbid = helper.mbidFromURL();
     fetch(`//acousticbrainz.org/api/v1/${mbid}/count`).then(
@@ -32,11 +37,11 @@ function showAcousticBrainzData() {
                 resp => resp.json()
             ).then(data => {
                 document.getElementById('ABkey').append(
-                    `${data.tonal.key_key} ${data.tonal.key_scale} (${_.round(
+                    `${data.tonal.key_key} ${data.tonal.key_scale} (${round(
                         100 * data.tonal.key_strength, 1)}%)`
                 );
-                document.getElementById('ABfreq').append(_.round(data.tonal.tuning_frequency, 2));
-                document.getElementById('ABbpm').append(_.round(data.rhythm.bpm, 1));
+                document.getElementById('ABfreq').append(round(data.tonal.tuning_frequency, 2));
+                document.getElementById('ABbpm').append(round(data.rhythm.bpm, 1));
                 document.getElementById('ABbeatcount').append(data.rhythm.beats_count);
             });
         }

mb-edit-merge_from_acoustid.user.js
@@ -1,10 +1,10 @@
-/* global $ _ helper requests sidebar */
+/* global $ helper requests sidebar */
 'use strict';
 // ==UserScript==
 // @name         MusicBrainz edit: Display acoustIDs and merge recordings with common acoustID
 // @namespace    mbz-loujine
 // @author       loujine
-// @version      2019.9.22
+// @version      2020.9.8
 // @downloadURL  https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mb-edit-merge_from_acoustid.user.js
 // @updateURL    https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mb-edit-merge_from_acoustid.user.js
 // @supportURL   https://github.com/loujine/musicbrainz-scripts
@@ -72,7 +72,7 @@ function showAcoustids() {
         }
         var duplicate_ids = Object.keys(ids).filter(
             // true if distinct recordings use the same acoustID
-            acid => _.uniq(ids[acid]).length > 1
+            acid => [...new Set(ids[acid])].size > 1
         );
         duplicate_ids.forEach(function (acid) {
             $('#acidForMerge').append(

mb-reledit-copy_dates.user.js
@@ -1,10 +1,10 @@
-/* global $ _ MB server relEditor GM_info */
+/* global $ MB server relEditor GM_info */
 'use strict';
 // ==UserScript==
 // @name         MusicBrainz relation editor: Copy dates on recording relations
 // @namespace    mbz-loujine
 // @author       loujine
-// @version      2020.4.12
+// @version      2020.9.8
 // @downloadURL  https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mb-reledit-copy_dates.user.js
 // @updateURL    https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mb-reledit-copy_dates.user.js
 // @supportURL   https://github.com/loujine/musicbrainz-scripts
@@ -41,7 +41,7 @@ function referenceDate(relations) {
     for (const unit of ['day', 'month', 'year']) {
         for (const [idx, rel] of relations.entries()) {
             if (rel.end_date[unit]() > 0
-                && _.values(server.recordingLinkType).includes(
+                && Object.values(server.recordingLinkType).includes(
                     parseInt(rel.linkTypeID()))) {
                 return idx;
             }
@@ -58,7 +58,7 @@ function propagateDates() {
         if (idx !== -1) {
             relations.forEach(function(rel) {
                 var linkType = parseInt(rel.linkTypeID());
-                if (!rel.removed() && _.values(server.recordingLinkType).includes(linkType)) {
+                if (!rel.removed() && Object.values(server.recordingLinkType).includes(linkType)) {
                     copyDate(relations[idx], rel);
                 }
             });

mbz-loujine-common.js
@@ -1,10 +1,10 @@
-/* global _ MB */
+/* global MB */
 'use strict';
 // ==UserScript==
 // @name         mbz-loujine-common
 // @namespace    mbz-loujine
 // @author       loujine
-// @version      2020.5.8
+// @version      2020.9.8
 // @description  musicbrainz.org: common functions
 // @compatible   firefox+greasemonkey
 // @license      MIT
@@ -320,7 +320,7 @@ class Server {
     getRelationshipAttrInfo() {
         const scriptSrc = document.scripts[document.scripts.length - 1].text;
         const jsonSource = new RegExp(/RE.exportTypeInfo\(\n(.*),\n(.*)\n/).exec(scriptSrc);
-        return _.values(JSON.parse(jsonSource[2]));
+        return Object.values(JSON.parse(jsonSource[2]));
     }
 
     getInstrumentRelationshipAttrInfo() {
@@ -343,13 +343,13 @@ const server = new Server();
 function buildOptions(obj) {
     // to be replaced by Object.entries when all supported browser versions
     // recognize it
-    return _.toPairs(obj).map(
+    return Object.entries(obj).map(
         ([type, code]) => `<option value="${code}">${type}</option>`
     ).join('');
 }
 
 function buildLanguageOptions(obj) {
-    return _.toPairs(obj).map(
+    return Object.entries(obj).map(
         ([type, code]) => `<option class="language" value="${code}">${type}</option>`
     ).join('');
 }
@@ -570,7 +570,7 @@ class Edits {
     }
 
     formatEdit(editType, info) {
-        return _.toPairs(info).map(
+        return Object.entries(info).map(
             ([prop, val]) => val === null ? `${editType}.${prop}`
                                           : `${editType}.${prop}=${val}`
         ).join('&');
@@ -629,13 +629,17 @@ class Helper {
         return this._isEntityTypeURL('work')
     }
 
+    sortBy = (key) => {
+    return (a, b) => (a[key] > b[key]) ? 1 : ((b[key] > a[key]) ? -1 : 0);
+    }
+
     sortSubworks(work) {
         let rels = work.relationships;
         rels = rels.filter(rel =>
             (rel.linkTypeID === server.workLinkType.subwork
              && rel.direction !== 'backward')
         );
-        rels = _.sortBy(rels, rel => rel.linkOrder);
+        rels = rels.sort(sortBy('linkOrder'));
         return rels.map(rel => rel.target);
     }
 }

commit c6ca4b73f659d51af09188149628251aa1a308a0

[wikidata] Fix bug on external links filling
Lodash removal in 77aa2395 introduced a bug, i.e. return is called in the first iteration on url '' and other links are not added

mb-edit-create_from_wikidata.user.js
@@ -4,7 +4,7 @@
 // @name         MusicBrainz edit: Create entity or fill data from wikipedia / wikidata / VIAF / ISNI
 // @namespace    mbz-loujine
 // @author       loujine
-// @version      2020.9.15
+// @version      2020.11.15
 // @downloadURL  https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mb-edit-create_from_wikidata.user.js
 // @updateURL    https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mb-edit-create_from_wikidata.user.js
 // @supportURL   https://github.com/loujine/musicbrainz-scripts
@@ -560,7 +560,7 @@ function _fillFormFromWikidata(entity, entityType) {
     for (const [externalLink, url] of Object.entries(libWD.urls)) {
         const domain = url.split('/')[2];
         if (!libWD.existField(entity, externalLink)) {
-            return;
+            continue;
         }
         const fullUrl = url + libWD.fieldValue(entity, externalLink);
         if (
@@ -763,6 +763,9 @@ $(document).ready(function () {
 
 // test data for places:
 // https://www.wikidata.org/wiki/Q2303621
+
+// test data for works:
+// https://www.wikidata.org/wiki/Q31745949
 
 // isni
 // http://www.isni.org/isni/0000000073684002 person