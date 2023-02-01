4c8dfd9aa3c800093aa141ab617d772a78261041
loujine <loujine@protonmail.com>
2023-02-01 23:59:41 +0000

    Fix operabase URL scheme (close #56)

--- mb-edit-create_from_wikidata.user.js
+++ mb-edit-create_from_wikidata.user.js
@@ -4,7 +4,7 @@
 // @name         MusicBrainz edit: Create entity or fill data from wikipedia / wikidata / VIAF / ISNI
 // @namespace    mbz-loujine
 // @author       loujine
-// @version      2022.1.1
+// @version      2023.2.2
 // @downloadURL  https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mb-edit-create_from_wikidata.user.js
 // @updateURL    https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mb-edit-create_from_wikidata.user.js
 // @supportURL   https://github.com/loujine/musicbrainz-scripts
@@ -131,7 +131,7 @@ class WikiDataHelpers {
             idGenius: 'https://genius.com/artists/',
             idSecondHandSongs: 'https://secondhandsongs.com/artist/',
             idVGMDB: 'https://vgmdb.net/artist/',
-            idOperabase: 'http://operabase.com/a/',
+            idOperabase: 'http://operabase.com/artists/',
             idSNAC: 'http://snaccooperative.org/ark:/99166/',
             // social media
             idTwitter: 'https://twitter.com/',
